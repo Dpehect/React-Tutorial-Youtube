e0f0ddadefba55dd18c51d3f495a519d43db2759
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2023-03-25 01:44:08 +0300

    Update useEffect.js

--- useEffect.js
+++ useEffect.js
@@ -1,43 +1,29 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("");
+function ExampleComponent() {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Bileşen oluşturulduğunda çalışacak kod
-    console.log("Bileşen oluşturuldu.");
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }, []);
-
-  useEffect(() => {
-    // count durumu güncellendiğinde çalışacak kod
-    console.log("count güncellendi.");
-  }, [count]);
-
-  useEffect(() => {
-    // message durumu güncellendiğinde çalışacak kod
-    console.log("message güncellendi.");
-  }, [message]);
-
-  const incrementCount = () => {
-    setCount(count + 1);
-    setMessage("Count arttırıldı.");
-  };
-
-  const decrementCount = () => {
-    setCount(count - 1);
-    setMessage("Count azaltıldı.");
-  };
 
   return (
     <div>
-      <h2>Counter</h2>
-      <p>Count: {count}</p>
-      <p>Message: {message}</p>
-      <button onClick={incrementCount}>Arttır</button>
-      <button onClick={decrementCount}>Azalt</button>
+      <h1>User List</h1>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Counter;
+export default ExampleComponent;
